c9442b22c17685f01a288e869ca8ade179570364
Anthony Catel <paraboul@gmail.com>
2025-01-04 15:07:55 +0100

    WIP: implement Server

--- pkg/libapenetwork/main.zig
+++ pkg/libapenetwork/main.zig
@@ -1,5 +1,57 @@
 pub const c = @import("c.zig").c;
+const std = @import("std");
+
+pub fn callAsync(comptime callback: anytype, args: anytype) void {
+    const gape = c.APE_get();
+
+
+    const wrapper = struct {
+        fn wrappedCallback(arg: ?*anyopaque) callconv(.C) c_int {
+            return @call(.always_inline, callback, .{@as(@TypeOf(args), @ptrCast(@alignCast(arg)))});
+        }
+    };
+
+    _ = c.APE_async(gape, wrapper.wrappedCallback, @constCast(args));
+}
+
+pub const Server = struct {
+    const Self = @This();
+
+    socket : [*c]c.ape_socket = null,
+
+
+    pub fn init() !Server {
+        return .{
+            .socket = c.APE_socket_new(c.APE_SOCKET_PT_TCP, 0, c.APE_get())
+        };
+    }
+
+    pub fn start(self: *Self, port: u16, comptime connected: anytype) !void {
+
+        if (c.APE_socket_listen(self.socket, port, "0.0.0.0", 0, 0) == -1) {
+            return error.APE_socket_listen_error;
+        }
+
+        self.socket.*.callbacks.arg = self;
+        self.socket.*.callbacks.on_connect = struct {
+            fn callback(_: [*c]c.ape_socket, _: [*c]c.ape_socket, _: [*c]c.ape_global, srv: ?*anyopaque) callconv(.C) void {
+
+                const ctx : *Self = @ptrCast(@alignCast(srv));
+
+                @call(.always_inline, connected, .{ ctx });
+            }
+        }.callback;
+    }
+};
+
+
+
+pub fn startLoop() void {
+    c.APE_loop_run(c.APE_get());
+}
 
 pub fn init() void {
     _ = c.APE_init();
+
+    std.debug.print(("APE init"), .{});
 }
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,35 @@
 const std = @import("std");
 const apenetwork = @import("libapenetwork");
 
+
+fn testcall(_: ?[]const u8) void {
+    std.debug.print("Async call\n", .{});
+}
+
+fn testcallbool(val: * const bool) u32 {
+    std.debug.print("Async call for bool {}\n", .{val.*});
+
+    return 10;
+}
+
+pub fn connected(_: *apenetwork.Server) void {
+    std.debug.print("New client connected", .{});
+}
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
     apenetwork.init();
+
+    std.debug.print("TYpe {}\n", .{@TypeOf(.{})});
+
+    const testref : bool = true;
+
+    apenetwork.callAsync(testcallbool, &testref);
+
+    var server = try apenetwork.Server.init();
+    try server.start(800, connected);
+
+    apenetwork.startLoop();
 }
